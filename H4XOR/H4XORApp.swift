a5d4c1ce80f62c391d323f233f35d37d2930e29a
omar assidi <omar.assidi@cmeoffshore.com>
2023-10-29 13:46:25 +0200

    added project

---
+++ H4XOR/H4XORApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XORApp.swift
+//  H4XOR
+//
+//  Created by OmarAssidi on 29/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct H4XORApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
